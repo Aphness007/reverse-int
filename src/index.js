aa7e7a4349ebbe83b72f615b1ceea05f58797c7a
marina <aphness007@gmail.com>
2023-08-25 12:53:16 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    // let reverse = String(n).split("");
+    // if(n < 0) reverse.shift();
+    // return +reverse.reverse().join("");
+    let reverse = [...String(Math.abs(n))];
+    return +reverse.reverse().join("");
 }
